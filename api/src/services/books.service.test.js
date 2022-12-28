c35c0e7afb931df243a7a5ab09977dcdee2cf601
Irving Juárez <irvingjuarez274@gmail.com>
2022-12-28 17:49:09 -0600

    First data mocking on the books service

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,5 +1,23 @@
 const BooksService = require("./books.service");
 
+const fakeBooks = [
+	{
+		_id: 1,
+		name: "21 lessons for the XXI century"
+	},
+	{
+		_id: 2,
+		name: "Population explosion"
+	}
+]
+
+const MongoLibStub = {
+	getAll: () => fakeBooks,
+	create: () => {}
+}
+
+jest.mock("../lib/mongo.lib", () => jest.fn().mockImplementation(() => MongoLibStub))
+
 describe("Test for the Books service", () => {
 	let service
 
@@ -12,11 +30,10 @@ describe("Test for the Books service", () => {
 
 		test("Making sure the books.length is correct", async () => {
 			// Act - Making an action with the exported service
-			const books = await service.getBooks()
-			console.log({ books })
+			const books = await service.getBooks();
 
 			// Assert
-			expect(books.length).toEqual();
+			expect(books.length).toEqual(2);
 		})
 	})
 })
